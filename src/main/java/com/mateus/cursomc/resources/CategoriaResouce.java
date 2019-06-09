62763be09f34f57528f147b976c4f034a207e296
mateusrdg <mateusrodrigues-cp@hotmail.com>
2019-06-09 18:01:52 -0300

    Resource

---
+++ src/main/java/com/mateus/cursomc/resources/CategoriaResouce.java
@@ -0,0 +1,14 @@
+package com.mateus.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResouce {
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar () {
+		return "troios";
+	}
+}
