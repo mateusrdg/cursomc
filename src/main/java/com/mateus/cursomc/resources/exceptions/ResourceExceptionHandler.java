b1ff1d094f0b69575429cdf5002ff1f7823246bf
mateusrdg <mateusrodrigues-cp@hotmail.com>
2019-06-12 23:31:34 -0300

    Estado e Cidade

--- src/main/java/com/mateus/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mateus/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -12,8 +12,10 @@ import com.mateus.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
