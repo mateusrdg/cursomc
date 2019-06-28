252d60994773c3f5bb2e909ee4f47b8e2438545f
mateusrdg <mateusrodrigues-cp@hotmail.com>
2019-06-28 00:10:43 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/mateus/cursomc/repositories/ClienteRepository.java
@@ -1,4 +1,4 @@
-package com.mateus.cursomc.repositories;
+ package com.mateus.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
